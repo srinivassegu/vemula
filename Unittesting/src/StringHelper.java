32d2695ec8726f132696dd7df62094b62ff471df
test <test@ivc0913adm.home>
2015-04-27 15:07:04 -0500

    3rd commit

--- Unittesting/src/StringHelper.java
+++ Unittesting/src/StringHelper.java
@@ -5,7 +5,7 @@ public class StringHelper {
 		{
 		if(str.length()<=2)	
 		return str.replaceAll("A", "");
-		String first2Chars=str.substring(0, 3);
+		String first2Chars=str.substring(0, 4);
 String stringMinusFirst2Chars=str.substring(2);
 return first2Chars.replaceAll("A","") + stringMinusFirst2Chars;
 	}
